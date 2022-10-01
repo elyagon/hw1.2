71bea2d052135fc6522d5e36126c76133902d250
elyagon <ehvelina-gg@yandex.ru>
2022-10-02 00:24:08 +0300

    Домашка 4-5

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        // Задание 1
-        System.out.println("Задача 1");
-        var dog = 8;
-        System.out.println(dog);
-        var cat = 3.6;
-        System.out.println(cat);
-        var paper = 763789;
-        System.out.println(paper);
-        //Задание 2
-        System.out.println("Задача 2");
-        dog = dog+4;
-        System.out.println(dog);
-        cat = cat + 4;
-        System.out.println(cat);
-        paper = paper + 4;
-        System.out.println(paper);
-        //Задача 3
-        System.out.println("Задача 3");
-        var dog1 = 8.0;
-        dog1 = dog1 - 3.5;
-        System.out.println(dog1);
-        cat = cat - 1.6;
-        System.out.println(cat);
-        paper = paper - 7639;
-        System.out.println(paper);
+        // Задание 4
+        System.out.println("Задача 4");
+        var friend =19;
+        System.out.println(friend);
+        friend = friend + 2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
+        //Задание 5
+        System.out.println("Задача 5");
+        var frog = 3.5;
+        System.out.println(frog);
+        frog = frog * 10;
+        System.out.println(frog);
+        frog = frog / 3.5;
+        System.out.println(frog);
+        frog = frog + 4;
+        System.out.println(frog);
 
     }
 }
